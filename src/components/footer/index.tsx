6e3bdec70d647ffb608c21c454bead42c484309e
wesleyricardi <wesley14_leandro@hotmail.com>
2021-09-07 15:33:33 -0300

    melhorias aplicada para mobile

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,8 +3,7 @@ import style from "../../styles/components/footer/index.module.css";
 export default function Footer() {
   return (
     <footer id="footer" className={style.main}>
-      Todos os direitos reservados © 2021 - Design by{" "}
-      <span>Wesley Ricardi</span>
+      © 2021 - Design by <span>Wesley Ricardi</span>
     </footer>
   );
 }
